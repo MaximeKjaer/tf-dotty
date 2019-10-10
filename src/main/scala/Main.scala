2cc68275f11afc66b4dbc4f423319b7d832e4a43
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-10-10 10:51:32 +0200

    Move experiments from Main to a worksheet

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,5 @@
 object Main {
-  import tensorflow.api.core._
-  val scalar = Shape.scalar
-  val vector = Shape.vector(20)
-  val matrix1 = Tensor[Int](Shape.matrix(10, 20))
-  val matrix1Bool = Tensor[Boolean](Shape.matrix(10, 20))
-  val matrix2 = Tensor[Int](Shape.matrix(20, 10))
-
   def main(args: Array[String]): Unit = {
-    val x = matrix1 + matrix1 - matrix1
-    val xBool = ~matrix1Bool ^ matrix1Bool | matrix1Bool & matrix1Bool
-    // val y = matrix1 * matrix2
+    
   }
 }
