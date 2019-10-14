d41a31ce801ef15ea5a2d455448a68dfac786891
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-10-14 10:54:23 +0200

    Move Tensor shape exclusively to type level

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
+import tensorflow.api.core._
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val matrix1 = Tensor[Int, 1 #: 2 #: 3 #: SNil]
+    val matrix2 = Tensor[Int, 3 #: 2 #: 1 #: SNil]
+    val matrixBool1 = Tensor[Boolean, 1 #: 4 #: SNil]
+    val matrixBool2 = Tensor[Boolean, 2 #: 2 #: SNil]
+
+    matrix1 + matrix1 - matrix1
+    ~matrixBool1 & matrixBool1
     
+    // Should not compile:
+    // matrix1 + matrix2
+    // matrix2 + matrix1
+    // ~matrix1
   }
 }
--- src/main/scala/tensorflow/api/core/Tensor.scala
+++ src/main/scala/tensorflow/api/core/Tensor.scala
@@ -1,44 +1,42 @@
 package tensorflow.api.core
 
-class Tensor[T : TFEncoding](val shape: Shape) {
-    type S = shape.type
+import scala.compiletime.constValue
 
+class Tensor[T : TFEncoding, S <: Shape] {
     /** Element-wise transformations result in a new tensor with the same dimensions */
-    def unary_- : Tensor.Aux[T, S] = Tensor[T](shape)
-    def abs: Tensor.Aux[T, S] = Tensor[T](shape)
+    def unary_- : Tensor[T, S] = new Tensor[T, S]
+    def abs: Tensor[T, S] = new Tensor[T, S]
 
     /* Element-wise operations result in a new tensor with the same dimensions */
-    def +(that: Tensor.Aux[T, S]): Tensor.Aux[T, S] = Tensor[T](shape)
-    def -(that: Tensor.Aux[T, S]): Tensor.Aux[T, S] = Tensor[T](shape)
-    def /(that: Tensor.Aux[T, S]): Tensor.Aux[T, S] = Tensor[T](shape)
-    def **(that: Tensor.Aux[T, S]): Tensor.Aux[T, S] = Tensor[T](shape)
-    def floorDiv(that: Tensor.Aux[T, S]): Tensor.Aux[T, S] = Tensor[T](shape)
+    def +(that: Tensor[T, S]): Tensor[T, S] = new Tensor[T, S]
+    def -(that: Tensor[T, S]): Tensor[T, S] = new Tensor[T, S]
+    def /(that: Tensor[T, S]): Tensor[T, S] = new Tensor[T, S]
+    def **(that: Tensor[T, S]): Tensor[T, S] = new Tensor[T, S]
+    def floorDiv(that: Tensor[T, S]): Tensor[T, S] = new Tensor[T, S]
 
     /** Element-wise logical functions for boolean tensors */ 
-    def unary_~(implicit ev: T <:< Boolean) : Tensor.Aux[T, S] = Tensor[T](shape)
-    def &(that: Tensor.Aux[Boolean, S])(implicit ev: T <:< Boolean): Tensor.Aux[Boolean, S] = Tensor[Boolean](shape)
-    def |(that: Tensor.Aux[Boolean, S])(implicit ev: T <:< Boolean): Tensor.Aux[Boolean, S] = Tensor[Boolean](shape)
-    def ^(that: Tensor.Aux[Boolean, S])(implicit ev: T <:< Boolean): Tensor.Aux[Boolean, S] = Tensor[Boolean](shape)
+    def unary_~(implicit ev: T <:< Boolean) : Tensor[T, S] = new Tensor[T, S]
+    def &(that: Tensor[Boolean, S])(implicit ev: T <:< Boolean): Tensor[Boolean, S] = new Tensor[Boolean, S]
+    def |(that: Tensor[Boolean, S])(implicit ev: T <:< Boolean): Tensor[Boolean, S] = new Tensor[Boolean, S]
+    def ^(that: Tensor[Boolean, S])(implicit ev: T <:< Boolean): Tensor[Boolean, S] = new Tensor[Boolean, S]
 
     /** Element-wise comparison results in a boolean tensor with the same dimensions */
-    def lt(that: Tensor.Aux[T, S]): Tensor.Aux[Boolean, S] = Tensor[Boolean](shape)
-    def le(that: Tensor.Aux[T, S]): Tensor.Aux[Boolean, S] = Tensor[Boolean](shape)
-    def ge(that: Tensor.Aux[T, S]): Tensor.Aux[Boolean, S] = Tensor[Boolean](shape)
-    def gt(that: Tensor.Aux[T, S]): Tensor.Aux[Boolean, S] = Tensor[Boolean](shape)
+    def lt(that: Tensor[T, S]): Tensor[Boolean, S] = new Tensor[Boolean, S]
+    def le(that: Tensor[T, S]): Tensor[Boolean, S] = new Tensor[Boolean, S]
+    def ge(that: Tensor[T, S]): Tensor[Boolean, S] = new Tensor[Boolean, S]
+    def gt(that: Tensor[T, S]): Tensor[Boolean, S] = new Tensor[Boolean, S]
+
+    // def reshape[NewShape <: Shape](given ev: Shape.NumElements[S] <:< Shape.NumElements[NewShape]): Tensor[T, NewShape] = new Tensor[T, NewShape]
 }
 
-object Tensor {
-    type Aux[T, S2] = Tensor[T] { type S = S2 }
-    
-    def apply[T : TFEncoding](shape: Shape): Tensor.Aux[T, shape.type] = 
-        new Tensor[T](shape).asInstanceOf[Tensor.Aux[T, shape.type]]
+object Tensor {    
+    def apply[T : TFEncoding, S <: Shape]: Tensor[T, S] = new Tensor[T, S]
 
-    def zeros[T : TFEncoding](shape: Shape): Tensor.Aux[T, shape.type] =
-        fill(shape)(summon[TFEncoding[T]].dataType.zero)
-
-    def fill[T : TFEncoding](shape: Shape)(value: T): Tensor.Aux[T, shape.type] = {
+    inline def zeros[T : TFEncoding, S <: Shape]: Tensor[T, S] = fill[T, S](summon[TFEncoding[T]].dataType.zero)
+    inline def fill[T : TFEncoding, S <: Shape](value: T): Tensor[T, S] = {
         val dataType = summon[TFEncoding[T]].dataType
-        val numBytes = shape.numElements * dataType.byteSize
+        val shape = constValue[S]
+        // val numBytes = shape.numElements * dataType.byteSize
         null // TODO
     }
 }
