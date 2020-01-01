611b5c5fe3fbde0bc951d424e9de8ffa5d43d6c6
Maxime Kjaer <maxime.kjaer@gmail.com>
2020-01-01 18:18:38 +0100

    Move ShapeOf to HLists.scala

--- modules/tensorflow/src/main/scala/ch/epfl/tensorflow/api/core/HLists.scala
+++ modules/tensorflow/src/main/scala/ch/epfl/tensorflow/api/core/HLists.scala
@@ -160,6 +160,27 @@ final case class #:[H <: Dimension, T <: Shape](head: H, tail: T) extends Shape 
     }
 }
 
+trait ShapeOf[T <: Shape] {
+    def value: T
+}
+
+object ShapeOf {
+    given ShapeOf[SNil.type] {
+        def value = SNil
+    }
+
+    given ShapeOf[SNil] {
+        def value = SNil
+    }
+
+    given [H <: Dimension, T <: Shape](given head: ValueOf[H], tail: ShapeOf[T]): ShapeOf[H #: T] {
+        def value = head.value #: tail.value
+    }
+}
+
+inline def shapeOf[S <: Shape](given s: ShapeOf[S]): S = s.value
+
+
 ////////////
 // Select //
 ////////////
